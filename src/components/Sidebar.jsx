792bad0e7d72f7a8edcc1e1e3f53cd5260b3fb1a
mingray_hsu <mingray_hsu@perfectcorp.com>
2024-08-17 16:20:44 +0800

    Update the Sidebar UI style and global css settings.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import sunIcon from '../assets/sun-sm.svg';
+import popIcon from '../assets/population-sm.svg';
 
 const Sidebar = () => {
+  const [selected, setSelected] = useState("/");
+
+  const getSelectedStyle = (path) => {
+    return selected === path ? 'text-[rgba(0,0,0,0.87)] bg-customBlue border-r-4 border-[#16538E]' : 'text-[rgba(0,0,0,0.4)]';
+  };
+
   return (
-    <aside className="w-64 bg-gray-800 text-white p-4">
-      <nav>
-        <ul>
-          <li className="mb-2">
-            <Link to="/" className="text-white">Today's Weather</Link>
+    <aside className="w-60 h-full flex-shrink-0 border-r border-gray-100 bg-white">
+      <nav className="mt-[42px] flex justify-center align-center">
+        <ul className="w-full">
+          <li className={"flex cursor-pointer px-8 py-4 text-[16px] font-normal leading-normal " + getSelectedStyle('/')}
+            onClick={() => setSelected("/")}
+          >
+            <img src={sunIcon} alt="sun-icon" className={"w-4 h-4 mr-3 " + (selected === '/' ? '' : 'opacity-40')} />
+            <Link to="/" className="" >Today's Weather</Link>
           </li>
-          <li className="mb-2">
-            <Link to="/population" className="text-white">Population</Link>
+          <li className={"flex cursor-pointer px-8 py-4 text-[16px] font-normal leading-normal " + getSelectedStyle('/population')}
+            onClick={() => setSelected("/population")}
+          >
+            <img src={popIcon} alt="pop-icon" className={"w-4 h-4 mr-3 " + (selected === '/population' ? '' : 'opacity-40')} />
+            <Link to="/population" className="" >Population</Link>
           </li>
         </ul>
       </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlue: 'rgba(22, 83, 142, 0.10)',
+      },
+    },
   },
   plugins: [],
 }
